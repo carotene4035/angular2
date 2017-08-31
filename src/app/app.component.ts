138906a7cfca0eb838babd24bf8931c94e32a1c0
carotene512 <karoten512@gmail.com>
2017-08-31 23:00:23 +0900

    クリックイベント実装、繰り返しイベント実装、選択時色替え実装。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Task } from  './Task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+  // そのコンポーネントが持っている
+  // プロパティを宣言していく
+  title: string = 'todo app';
+  task: Task = {
+    id: 1,
+    name: '田中さんにメール'
+  };
+  tasks = TASKS;
+  selectedTask: Task;
+
+  // 渡されたtaskクラスを引数として受け取る
+  onSelect(task: Task): void {
+    this.selectedTask = task;
+  }
 }
+
+// 定数として管理
+const TASKS: Task[] = [
+  {id: 11, name: 'hahaha'},
+  {id: 12, name: 'hahaha'},
+  {id: 13, name: 'hahaha'},
+  {id: 14, name: 'hahaha'},
+];
