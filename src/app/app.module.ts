6e924442ad56020c5c70bcd4e5a18200c6d792f4
carotene512 <karoten512@gmail.com>
2017-09-01 00:03:54 +0900

    親子コンポーネントに切り分け

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule,
