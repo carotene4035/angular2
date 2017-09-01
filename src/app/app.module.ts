f705c33cf15e4fe115d22ab5554c8daf74f6584b
carotene512 <karoten512@gmail.com>
2017-09-01 13:45:16 +0900

    routingの追加。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
@@ -16,7 +17,13 @@ import { TasksComponent } from './tasks/tasks.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'list',
+        component: TasksComponent
+      }
+    ])
   ],
   providers: [
     TaskService
